b2a04ea30656e8911cd7dd0ba27a0b2e77d274a5
HNHSW <laverna.trouble@proton.me>
2024-10-21 14:11:48 +0200

    udr change ressource to data

--- udr.tf
+++ udr.tf
@@ -1,16 +1,13 @@
-# Utiliser une data pour récupérer la table de routage UDR existante
 data "azurerm_route_table" "udr" {
   name                = "UDR"
   resource_group_name = "Testrg"
 }
 
-# Associer la table de routage UDR à ton premier sous-réseau
 resource "azurerm_subnet_route_table_association" "subnet_assoc_route_table_1" {
   subnet_id      = azurerm_subnet.subnet1.id
   route_table_id = data.azurerm_route_table.udr.id
 }
 
-# Associer la table de routage UDR à ton deuxième sous-réseau
 resource "azurerm_subnet_route_table_association" "subnet_assoc_route_table_2" {
   subnet_id      = azurerm_subnet.subnet2.id
   route_table_id = data.azurerm_route_table.udr.id  
